34551f6f45ebd76fba9a8512c4301b83082c37d6
Pete Mertz <peter.s.mertz@gmail.com>
2016-02-20 20:53:01 -0500

    Make generate pair method

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,7 +4,8 @@ var crypto = require('crypto');
 var redis = require('./redis');
 
 module.exports = {
-  verify: verify
+  verify: verify,
+  generatePair: generatePair
 };
 
 
@@ -14,11 +15,29 @@ function _hash(input) {
   return shasum.digest('hex');
 }
 
+function generatePair() {
+  const apiKey = crypto.randomBytes(Math.ceil(8)).toString('hex');
+  const secret = crypto.randomBytes(Math.ceil(16)).toString('hex');
+
+  return new Promise((resolve, reject) => {
+    redis.set(apiKey, secret, function(err) {
+      if (err) {
+        throw err;
+      }
+
+      resolve({
+        apiKey: apiKey,
+        secret: secret
+      });
+    });
+  });
+}
+
 function verify(apiKey, hash) {
   return new Promise((resolve, reject) => {
     redis.get(apiKey, function(err, secret) {
       if (err) {
-        return reject(err);
+        throw err;
       }
 
       if (hash != _hash(apiKey + secret)) {
